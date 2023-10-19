335cb6111dbabb1472aa8d0804587734d41d46ce
WJStephenson <willjosteph@gmail.com>
2023-10-19 17:31:21 +0100

    refuse entry for screens under 1000px wide

--- src/App.tsx
+++ src/App.tsx
@@ -14,23 +14,41 @@ import Settings from './Pages/Settings/Settings';
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
 
+  const [isMobile, setIsMobile] = useState<boolean>(false);
+
+  window.addEventListener('resize', () => {
+    setIsMobile(screen.width < 1000);
+  });
+
   return (
     <BrowserRouter>
       <ModalContextProvider>
         <div className='app-container'>
           {
-            isLoggedIn &&
-            <Search setIsLoggedIn={setIsLoggedIn}/>
+            isMobile ?
+
+              <div className='error-container'>
+                <h1>Sorry, Just Works was built for the desktop.</h1>
+                <h2>Please use a screen atleast 1000px wide to view this website.</h2>
+              </div>
+
+              :
+              <>
+                {
+                  isLoggedIn &&
+                  <Search setIsLoggedIn={setIsLoggedIn} />
+                }
+                <Routes>
+                  <Route path="/" element={isLoggedIn ? (<Homepage />) : (<Navigate to={isLoggedIn ? '/' : "/login"} />)} />
+                  <Route path="/completed" element={isLoggedIn ? (<CompletedJobs />) : (<Navigate to={isLoggedIn ? 'complete' : "/login"} />)} />
+                  <Route path='/calendar' element={isLoggedIn ? (<MyCalendar />) : (<Navigate to={isLoggedIn ? 'calendar' : "/login"} />)} />
+                  <Route path='/analytics' element={isLoggedIn ? (<Analytics />) : (<Navigate to={isLoggedIn ? 'analytics' : "/login"} />)} />
+                  <Route path="/login" element={isLoggedIn ? (<Navigate to="/" />) : (<Login setIsLoggedIn={setIsLoggedIn} />)} />
+                  <Route path="/settings" element={isLoggedIn ? (<Settings />) : (<Navigate to={isLoggedIn ? 'settings' : "/login"} />)} />
+                  <Route path="*" element={<Navigate to="/" />} />
+                </Routes>
+              </>
           }
-          <Routes>
-            <Route path="/" element={isLoggedIn ? (<Homepage />) : (<Navigate to={isLoggedIn ? '/' : "/login"} />)} />
-            <Route path="/completed" element={isLoggedIn ? (<CompletedJobs />) : (<Navigate to={isLoggedIn ? 'complete' : "/login"} />)} />
-            <Route path='/calendar' element={isLoggedIn ? (<MyCalendar />) : (<Navigate to={isLoggedIn ? 'calendar' : "/login"} />)} />
-            <Route path='/analytics' element={isLoggedIn ? (<Analytics />) : (<Navigate to={isLoggedIn ? 'analytics' : "/login"} />)} />
-            <Route path="/login" element={isLoggedIn ? (<Navigate to="/" />) : (<Login setIsLoggedIn={setIsLoggedIn} />)} />
-            <Route path="/settings" element={isLoggedIn ? (<Settings />) : (<Navigate to={isLoggedIn ? 'settings' : "/login"} />)} />
-            <Route path="*" element={<Navigate to="/" />} />
-          </Routes>
         </div>
       </ModalContextProvider>
     </BrowserRouter >
